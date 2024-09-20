076b3b9757a858815d17df7eed51f339c6420fdc
Padmanaban <padmanaban870@gmail.com>
2024-09-20 16:12:04 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ app.use("/api",Routes)
 app.use(express.static(path.join(__dirname,"/frontend/chatspace/dist")))
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
+	res.sendFile(path.join(__dirname, "frontend","chatspace","dist","index.html"));
 });
 
 server.listen(port,()=>{console.log(`Server is running on port ${port}`)})
