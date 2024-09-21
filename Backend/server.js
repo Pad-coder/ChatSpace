3ebf19c43ff3c0ad3cda3581bea0b441159d6fa7
Padmanaban <padmanaban870@gmail.com>
2024-09-21 19:08:01 +0530

    Frontend and backend connection created

--- Backend/server.js
+++ Backend/server.js
@@ -16,10 +16,10 @@ app.use(cookieParser())
 
 app.use("/api",Routes)
 
-app.use(express.static(path.join(__dirname, "/chatspace-Client/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "chatspace-Client", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
 
 server.listen(port,()=>{console.log(`Server is running on port ${port}`)})
